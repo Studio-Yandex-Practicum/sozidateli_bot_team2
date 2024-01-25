c587a940a2d65c30556fd6df3a08c0c713dbf6f8
pavlovvitaliy <me@pavlovvitaliy.ru>
2024-01-25 20:50:16 +0300

    Add redis settings to the bot.

--- bot/src/core/settings.py
+++ bot/src/core/settings.py
@@ -12,3 +12,4 @@ class Settings:
     throttle_time_spin: int = os.getenv('THROTTLE_TIME_SPIN')
     throttle_time_other: int = os.getenv('THROTTLE_TIME_OTHER')
     url: str = os.getenv('URL', 'http://localhost:8000')
+    redis_host = os.getenv("REDIS_HOST", "localhost")
--- bot/src/main.py
+++ bot/src/main.py
@@ -1,8 +1,8 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
+from aiogram.fsm.storage.redis import RedisStorage, Redis
 from aiogram_forms import dispatcher
 from core import settings
 from handlers import routers
@@ -10,10 +10,16 @@ from middlewares.throttling import ThrottlingMiddleware
 
 
 async def main():
+    redis = Redis(host=settings.redis_host)
+    storage = RedisStorage(redis=redis)
     bot = Bot(settings.bot_token, parse_mode='HTML')
-    dp = Dispatcher(storage=MemoryStorage())
-    dp.message.middleware(ThrottlingMiddleware(settings.throttle_time_spin,
-                                               settings.throttle_time_other))
+    dp = Dispatcher(storage=storage)
+    dp.message.middleware(
+        ThrottlingMiddleware(
+            settings.throttle_time_spin,
+            settings.throttle_time_other
+        )
+    )
     dp.callback_query.middleware(CallbackAnswerMiddleware())
     dispatcher.attach(dp)
 
@@ -21,8 +27,10 @@ async def main():
         dp.include_router(router)
 
     try:
-        await dp.start_polling(bot,
-                               allowed_updates=dp.resolve_used_update_types())
+        await dp.start_polling(
+            bot,
+            allowed_updates=dp.resolve_used_update_types()
+        )
     finally:
         await bot.session.close()
 
