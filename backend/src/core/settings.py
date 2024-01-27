7cc1d080460e64afbe585392b7e60dd616a78328
pavlovvitaliy <me@pavlovvitaliy.ru>
2024-01-27 12:01:09 +0300

    Fix users service and add deafault settings for admin.

--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -13,8 +13,8 @@ class Settings:
 
     db_url: str = os.getenv("DB_URL", "sqlite+aiosqlite:///sqlite.db")
     app_title = "API для проекта 'Созидатели'."
-    admin_panel_user = os.getenv("ADMIN_PANEL_USER")
-    admin_panel_password = os.getenv("ADMIN_PANEL_PASSWORD")
+    admin_panel_user = os.getenv("ADMIN_PANEL_USER", "admin")
+    admin_panel_password = os.getenv("ADMIN_PANEL_PASSWORD", "password")
     admin_middleware_secret = os.getenv(
         "ADMIN_MIDDLEWARE_SECRET", "1234567890"
     )
