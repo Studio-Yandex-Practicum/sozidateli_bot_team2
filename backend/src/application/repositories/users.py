46e9d37355020bffabbe5c67072151fff48f2ab4
AnfryWolf <qrer71@yandex.ru>
2024-01-28 20:10:06 +0300

    Update repositories

--- backend/src/application/repositories/users.py
+++ backend/src/application/repositories/users.py
@@ -1,6 +1,12 @@
+from sqlalchemy import select
+
 from src.application.protocols.repository import SQLAlchemyRepository
 from src.domain.models.users import User
 
 
 class UserRepository(SQLAlchemyRepository):
     model = User
+
+    async def check_user_exists(self, **filter_by) -> bool:
+        stmt = select(self.model).filter_by(**filter_by).exists()
+        return await self.session.scalar(select(stmt))
