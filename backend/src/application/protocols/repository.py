e40b6fc25ddb9e9a2aaef83bd05b78f0799d3fa4
AnfryWolf <qrer71@yandex.ru>
2024-01-28 20:09:17 +0300

    Optimize database queries

--- backend/src/application/protocols/repository.py
+++ backend/src/application/protocols/repository.py
@@ -1,11 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Any, Sequence, TypeVar
 
-from sqlalchemy import delete, insert, or_, select, update
+from sqlalchemy import delete, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.infrastructure.db import Base
-
 
 ModelType = TypeVar("ModelType", bound=Base)
 
@@ -16,7 +15,7 @@ class AbstractRepository(ABC):
         ...
 
     @abstractmethod
-    async def update_one(self):
+    async def update_one(self, id: int):
         ...
 
     @abstractmethod
@@ -39,49 +38,39 @@ class SQLAlchemyRepository(AbstractRepository):
         self.session = session
 
     async def add_one(self, **kwargs: dict[str, Any]) -> ModelType:
+        """Добавление данных в БД."""
         stmt = insert(self.model).values(**kwargs).returning(self.model)
         result = await self.session.execute(stmt)
         return result.scalar_one()
 
-    async def update_one(self, id: int, **kwargs: dict[str, Any]) -> ModelType:
-        kwargs = {
-            key: value for key, value in kwargs.items() if value is not None
-        }
-        stmt = (
-            update(self.model)
-            .values(**kwargs)
-            .filter_by(id=id)
-            .returning(self.model)
-        )
+    async def update_one(
+            self, id: int, **kwargs: dict[str, Any]
+    ) -> ModelType:
+        """Обновление данных в БД."""
+        stmt = update(
+            self.model
+        ).values(**kwargs).filter_by(id=id).returning(self.model)
         result = await self.session.execute(stmt)
         return result.scalar_one()
 
-    async def find_one(self, **filter_by: dict[str, Any]) -> ModelType | None:
-        filters = [
-            getattr(self.model, key) == value
-            for key, value in filter_by.items()
-        ]
-        stmt = select(self.model).filter(or_(*filters))
+    async def find_one(
+            self, **filter_by: dict[str, Any]
+    ) -> ModelType | None:
+        """Поиск записи в БД."""
+        stmt = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
     async def find_all(self) -> Sequence[ModelType]:
+        """Поиск записей в БД."""
         stmt = select(self.model)
         result = await self.session.execute(stmt)
         return result.scalars().all()
 
     async def delete_one(self, **filter_by: dict[str, Any]) -> ModelType:
-        stmt = delete(self.model).filter_by(**filter_by).returning(self.model)
+        """Удаление записи в БД."""
+        stmt = delete(
+            self.model
+        ).filter_by(**filter_by).returning(self.model)
         result = await self.session.execute(stmt)
         return result.scalar_one()
-
-    async def find_all_by_attrs(
-        self, **filter_by: dict[str, Any]
-    ) -> ModelType | None:
-        filters = [
-            getattr(self.model, key) == value
-            for key, value in filter_by.items()
-        ]
-        stmt = select(self.model).filter(or_(*filters))
-        result = await self.session.execute(stmt)
-        return result.scalars().all()
