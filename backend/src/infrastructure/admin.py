8853b227bda608fa27a0052713d9c6af620b3f3d
AnfryWolf <qrer71@yandex.ru>
2024-01-28 20:03:04 +0300

    Fix bugs

--- backend/src/infrastructure/admin.py
+++ backend/src/infrastructure/admin.py
@@ -1,6 +1,6 @@
+from dataclasses import dataclass
 from typing import Any, Dict
 
-from sqlalchemy import select
 from starlette.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
@@ -20,14 +20,22 @@ from starlette_admin.contrib.sqla.ext.pydantic import ModelView
 from starlette_admin.exceptions import FormValidationError
 from starlette_admin.i18n import I18nConfig
 
+from src.application.repositories.users import UserRepository
 from src.core import Settings as settings
 from src.domain.models import Meeting, User
-from src.domain.models.assistance import AssistanceSegment
+from src.domain.models.enums import AssistanceSegment
 from src.domain.schemas import MeetingCreate, UserCreate
-from src.infrastructure.db import async_session_maker
-
 from .db import engine
 from .provider import UsernameAndPasswordProvider
+
+
+@dataclass
+class EnumCustomField(EnumField):
+    def _get_label(self, value: Any, request: Request) -> Any:
+        for v, label in self._get_choices(request):
+            if value == getattr(AssistanceSegment, v):
+                return label
+        raise ValueError(f"Invalid choice value: {value}")
 
 
 class UserView(ModelView):
@@ -39,10 +47,15 @@ class UserView(ModelView):
         StringField("name", label="Имя", required=True),
         PhoneField("phone", label="Номер телефона", required=True),
         EmailField("email", required=True),
-        EnumField(
+        EnumCustomField(
             "assistance_segment",
             label="Направление помощи",
-            enum=AssistanceSegment,
+            choices=[("children_in_hospital", "Детям в больницах"),
+                     ("children_in_orphanages", "Детям в детских домах"),
+                     ("disabled_children", "Семьям с детьми-инвалидами"),
+                     ("auto_volunteer", "Могу автоволонтерить"),
+                     ("not_decide", "Еще не определился")],
+            required=True
         ),
         HasOne("meeting", label="Собрание", identity="meeting", required=True),
     ]
@@ -50,34 +63,46 @@ class UserView(ModelView):
     sortable_fields = [User.meeting]
 
     async def validate(self, request: Request, data: Dict[str, Any]) -> None:
+        errors: dict[str, str] = dict()
+        if data["assistance_segment"] is None:
+            errors["assistance_segment"] = "Нужно выбрать направление помощи."
         if data["meeting"] is None:
-            raise FormValidationError(
-                {"meeting": "Надо выбрать дату собрания."}
-            )
-        meeting = data.pop("meeting")
-        user_attrs = {
-            User.name: "name",
-            User.phone: "phone",
-            User.email: "email",
-        }
-        for attr, attr_name in user_attrs.items():
-            async with async_session_maker() as session:
-                users = await session.execute(
-                    select(User).where(attr == data[attr_name])
-                )
-            if users.scalars().all():
-                raise FormValidationError(
-                    {attr_name: "Уже есть участник с такими данными."}
-                )
-        data["meeting_id"] = meeting.id
+            errors["meeting"] = "Нужно выбрать собрание."
+        if data["phone"] is None:
+            errors["phone"] = "Нужно указать номер телефона."
+        if data["name"] is None:
+            errors["name"] = "Нужно указать имя участника."
+        if data["email"] is None:
+            errors["email"] = "Нужно укзать почту."
+
+        self._change_assistance_segment(data)
+        data["meeting_id"] = data["meeting"].id
+
+        if request.state.action == "CREATE":
+            await self._validate_create(request, data, errors)
+
+        if len(errors) > 0:
+            raise FormValidationError(errors)
+
         await super().validate(request, data)
-        if assistance_segment_value := data.get("assistance_segment"):
-            assistance_segment_key = AssistanceSegment(
-                assistance_segment_value
-            ).name
-            data["assistance_segment"] = assistance_segment_key
-        data.pop("meeting_id")
-        data["meeting"] = meeting
+
+    def _change_assistance_segment(self, data: dict[str, Any]):
+        data["assistance_segment"] = getattr(
+            AssistanceSegment, data["assistance_segment"]
+        )
+
+    async def _validate_create(
+            self, request: Request,
+            data: dict[str, Any],
+            errors: dict[str, str]
+    ):
+        user_attrs = {"email": "Пользователя с данной почтой уже существует.",
+                      "phone": "Пользователь с данным телефоном уже существует."}
+        for key, value in user_attrs.items():
+            if await UserRepository(
+                    request.state.session
+            ).check_user_exists(**{key: data[key]}):
+                errors[key] = value
 
 
 class MeetingView(ModelView):
