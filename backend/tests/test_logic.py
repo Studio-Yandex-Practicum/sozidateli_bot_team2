d16087f7d0c68ec6b494a42d875f8b91985486d6
Александр Русанов <shurik.82rusanov@yandex.ru>
2024-01-28 17:06:32 +0300

    4tests

--- backend/tests/test_logic.py
+++ backend/tests/test_logic.py
@@ -7,18 +7,23 @@ from src.application.services.users import UserService
 #from src.domain.models import User
 
 
-@pytest.mark.asyncio
-async def test_create_user():
-    async with AsyncClient(app=app, base_url='http://test') as client:
-        new_user_data: UserService = {
-            'id': 1,
-            'name': 'test',
-            'phone': '12345678990',
-            'email': 'a@a.ru',
-            'meeting_id': 1,
-            'assistance_segment': 'not_decide',
-            'meeting': '2022-01-01',
-        }
-        response = await client.post('/users/', json=new_user_data)
-        assert response.status_code == 201
-        assert response.json() == new_user_data
+@pytest.mark.parametrize('json', [
+  {"date": "2024-01-30T15:15:45.848Z",
+   "description": "Описание встречи"}
+])
+async def test_post_meeting(async_client: AsyncClient, json):
+    response = await async_client.post('/meetings/')
+    assert response.status_code == 200, 'Код запроса вернулся неверный!'
+    data = response.json()
+    keys = sorted(['id', 'date', 'is_open', 'description'])
+    assert keys == sorted(data.keys()), 'Ключи в ответе не совпадают'
+
+
+@pytest.mark.parametrize('json', [
+    {'name': 'test', 'phone': '12345678990',
+     'email': 'a@a.ru', 'meeting_id': 1,
+     'assistance_segment': 'Детям в детских домах'}
+])
+async def test_create_user(async_client: AsyncClient, json):
+    response = await async_client.post('/users/')
+    assert response.status_code == 200, 'Не удалось создать пользователя'
