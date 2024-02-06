6db982a9faf9027b3f6cc0b12cdda397ca949c40
AngryWolf <qrer71@yandex.ru>
2024-02-06 20:50:55 +0300

    Update constants

--- backend/app/core/constants.py
+++ backend/app/core/constants.py
@@ -3,3 +3,19 @@ DATE_FORMAT = "%d %m %Y %H:%M:%S"
 ZONEINFO = "Europe/Moscow"
 USERNAME_LENGTH = 4
 PASSWORD_LENGTH = 6
+URL = "https://sozidateli.spb.ru/volonteru/"
+DATE_PATTERN = r"\d{2}\s\w+\s\w+\s\d{2}\.\d{2}"
+MONTH = {
+    'января': 1,
+    'февраля': 2,
+    'марта': 3,
+    'апреля': 4,
+    'мая': 5,
+    'июня': 6,
+    'июля': 7,
+    'августа': 8,
+    'сентября': 9,
+    'октября': 10,
+    'ноября': 11,
+    'декабря': 12
+}
