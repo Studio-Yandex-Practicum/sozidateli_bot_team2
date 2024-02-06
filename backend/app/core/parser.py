a7417de3d034981023234494538d54dfa160bd7e
AngryWolf <qrer71@yandex.ru>
2024-02-06 20:51:56 +0300

    Create parser

---
+++ backend/app/core/parser.py
@@ -0,0 +1,30 @@
+import datetime as dt
+import re
+from http import HTTPStatus
+
+import httpx
+from lxml import html
+
+from app.core.constants import DATE_FORMAT, URL, MONTH, DATE_PATTERN
+
+
+async def parser_date() -> None | dt.datetime:
+    """Парсер даты с сайта."""
+    async with httpx.AsyncClient() as client:
+        response = await client.get(URL)
+        if response.status_code != HTTPStatus.OK:
+            return None
+        tree = html.fromstring(response.text)
+        text = tree.xpath(
+            "//div[@class='e-con-inner']"
+            "//p[@class='has-black-color has-white-background-color "
+            "has-text-color has-background']//strong",
+        )
+        try:
+            date_text = re.findall(DATE_PATTERN, text[0].text)[0].split()
+        except IndexError:
+            return None
+        date = (f'{date_text[0]} {MONTH[date_text[1]]} '
+                f'{dt.datetime.now().year} '
+                f'{date_text[-1].replace(".", ":")}:00')
+        return dt.datetime.strptime(date, DATE_FORMAT)
